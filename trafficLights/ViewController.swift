e20d7cbbe726f4bf3f9fac38904de6cde17021f9
rlt202 <danik.nikulin@gmail.com>
2020-10-14 01:27:37 +0300

    Готовая логика, но что-то мне подсказывает, что весьма кривая)

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var buttonForChanging: UIButton!
+    
+    private var actualSubView = Views.red
+    
+    enum Views {
+        case red
+        case yellow
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = redView.layer.cornerRadius
+        greenView.layer.cornerRadius = redView.layer.cornerRadius
+        
     }
-
-
+    
+    @IBAction func changingColor() {
+       
+        buttonForChanging.setTitle("Next color", for: .normal)
+        
+        switch actualSubView {
+        case .red:
+            redView.alpha = 1
+            greenView.alpha = 0.3
+            actualSubView = .yellow
+        case .yellow:
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+            actualSubView = .green
+        case .green:
+            greenView.alpha = 1
+            yellowView.alpha = 0.3
+            actualSubView = .red
+            buttonForChanging.setTitle("Rerun", for: .normal)
+        }
+    }
+    
 }
 
